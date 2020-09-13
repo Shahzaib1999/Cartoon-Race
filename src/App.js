777b1914600c67dc9251d5cf62f828d6f03a07a5
M.Shahzaib <mohdshahzaib24@gmail.com>
2020-09-14 04:43:16 +0500

    app completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import useWebAnimations from '@wellyshen/use-web-animations';
+
 import './App.css';
 
+import Cartoon from './assets/cartoon.gif';
+
 function App() {
+  const { ref, getAnimation } = useWebAnimations({
+    keyframes: [
+      { transform: 'translate(-100%,0)' },
+      { transform: 'translate(300%,0)' }
+    ],
+    timing: {
+      duration: 5000,
+      playbackRate: 1,
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  });
+
+  const speedUp = () => {
+    const animation = getAnimation();
+    animation.updatePlaybackRate(animation.playbackRate * 1.1);
+    setInterval(
+      () => {
+        if (animation.playbackRate > 1) {
+          animation.playbackRate *= .9;
+        }
+      }, 6000)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onClick={speedUp}>
+      <img src={Cartoon} className="img" ref={ref} alt="cartoon" />
     </div>
   );
 }
